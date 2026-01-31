224b060e1e9e51c15734b33783cfae34d5771baf
anand98765 <anand98765@gmail.com>
2026-01-31 20:42:42 +0530

    added Google analytics code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
+import Analytics from "./Analytics";
 
 export const metadata: Metadata = {
   title: "సహస్రచండి మరియు శివ దశసహస్రనామ మహాయజ్ఞం",
-  description: "జగన్మాత అమ్మవారి మరియు పరమేశ్వరుని దివ్య ఆశీర్వాదాల కోసం నిర్వహించబడే పవిత్ర వైదిక యజ్ఞం.",
+  description:
+    "జగన్మాత అమ్మవారి మరియు పరమేశ్వరుని దివ్య ఆశీర్వాదాల కోసం నిర్వహించబడే పవిత్ర వైదిక యజ్ఞం.",
 };
 
 export default function RootLayout({
@@ -13,7 +16,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="te">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-H7H4Y59F54"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H7H4Y59F54', {
+              send_page_view: false
+            });
+          `}
+        </Script>
+      </head>
+
       <body>
+        <Analytics />
         {children}
       </body>
     </html>
